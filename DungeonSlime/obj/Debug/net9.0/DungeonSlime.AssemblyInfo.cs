f4563aadcdeffb15da34db7122cf593acc08fffa
Gavin Brooks <gavintrey2005@gmail.com>
2026-09-01 19:15:27 -0400

    fixed issue booting

--- DungeonSlime/obj/Debug/net9.0/DungeonSlime.AssemblyInfo.cs
+++ DungeonSlime/obj/Debug/net9.0/DungeonSlime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2680b8dbc123ce2eddbb82b1d6fd0186ac281deb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6539a2557150dc7a1e03dcefc14d01e5c020a27a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MonoGameLibrary/obj/Debug/net9.0/MonoGameLibrary.AssemblyInfo.cs
+++ MonoGameLibrary/obj/Debug/net9.0/MonoGameLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+840faa4ac4e0a23a5f4ea79b2ed2cab06ce186ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6539a2557150dc7a1e03dcefc14d01e5c020a27a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

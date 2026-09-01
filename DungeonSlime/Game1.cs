6539a2557150dc7a1e03dcefc14d01e5c020a27a
Gavin Brooks <gavintrey2005@gmail.com>
2026-09-01 18:53:47 -0400

    integrated game library

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -1,44 +1,34 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
 
 namespace DungeonSlime;
 
-public class Game1 : Game
+public class Game1 : Core
 {
-    // the window that opens up when the game is run, this is where all the graphics will be drawn
-    private GraphicsDeviceManager _graphics;
-    // container that collects batches of sprites to be drawn to the screen, this is used to draw all the graphics in the game
-    private SpriteBatch _spriteBatch;
-
-    public Game1()
+    public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
-        //look more into this
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+
     }
 
     protected override void Initialize()
     {
-        // cannot use content loading object to collect data from image files, sounds, etc. until the game is initialized
+        // TODO: Add your initialization logic here
 
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
-        // Create a new SpriteBatch, which can be used to draw textures.
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
-
         // TODO: use this.Content to load your game content here
+
+        base.LoadContent();
     }
 
     protected override void Update(GameTime gameTime)
     {
-        //stores a snapshot of all the keys pressed at on the keyboard at a given moment
-        KeyboardState ks = Keyboard.GetState();
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || ks.IsKeyDown(Keys.Escape))
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
         // TODO: Add your update logic here
